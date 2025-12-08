c497342ef6e8ff96f20c265a676337dbd181730a
Admin-NIS <admin-nis@Admin-NISs-Mac-mini.local>
2025-12-08 10:31:56 +0530

    minor chnage

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 'use client';
-import Head from 'next/head';
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import AboutUs from '@/Components/Sections.tsx/AboutUs';
+
+import Navbar from '@/Components/Navbar';
+import Hero from '@/Components/Hero';
+import About from '@/Components/Sections/AboutUs';
+import Different from '@/Components/Sections/Different';
+import ContactSection from '@/Components/Sections/ContactSection';
+import Footer from '@/Components/Footer';
+
+export const metadata = {
+  title: "GreenRide - EV Cab Booking - Central India's Largest",
+  viewport: "width=device-width, initial-scale=1.0",
+};
 
 export default function Page() {
   return (
-    <>
-      <Head>
-        <title>GreenRide - EV Cab Booking - Central India's Largest</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="UTF-8" />
-      </Head>
-      <main className="scroll-smooth">
-        <Navbar />
-        <Hero />
-        <AboutUs />
-      </main>
-    </>
+    <main className="scroll-smooth">
+      <Navbar />
+      <Hero />
+      <About />
+      <Different />
+      <ContactSection />
+      <Footer />
+    </main>
   );
 }
